6e73a4a8218a67339a269ed892b234c1e5e12d74
coconut-irl <coconutx206@gmail.com>
2019-03-10 22:51:43 +0530

    Fixed typo

--- jack-jill.py
+++ jack-jill.py
@@ -1,4 +1,4 @@
 print("Jack and Jill went up a hill")
 print("to feth a pail of water;")
 print("Jack fell down, and broke his crown,")
-print("and jill came tumbling after.")
+print("and Jill came tumbling after.")
